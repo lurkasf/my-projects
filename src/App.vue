9b54701d8532efaec431f9bfd24854f68c3a5f4d
lucas.borges <lucas.borges@azi.com.br>
2020-05-05 12:46:25 -0400

    atualizando o codigo com algo

--- src/App.vue
+++ src/App.vue
@@ -29,11 +29,6 @@ export default {
           name: 'Anagrama',
           icon:'https://raw.githubusercontent.com/lurkasf/Anagrama/master/public/icon.png',
           description:'Jogo do Anagrama o qual consiste em descobrir qual é a palavra que está embaralhada'},
-        
-        {url: '#', name: 'Exemplo', description:'wtf, that´s an example', icon:'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg'},
-        {url: '#', name: 'Exemplo', description:'wtf, that´s an example', icon:'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg'},
-        {url: '#', name: 'Exemplo', description:'wtf, that´s an example', icon:'https://previews.123rf.com/images/fordzolo/fordzolo1506/fordzolo150600296/41026708-example-white-stamp-text-on-red-backgroud.jpg'},
-
       ]
     }
   }
